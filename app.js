3e3137d265584a76402d135772b7880cda4a125f
stefpi <stef@ceatanostra.org>
2019-04-14 13:25:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const wsc = new ws('ws://192.168.0.32:4001');
+const wsc = new ws('ws://ranviermudip');
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
